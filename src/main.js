97983b6e6522db58c3b7c6bf2c20b4cac3898411
Tessa Thornton <tessathornton@gmail.com>
2014-11-06 00:36:11 -0500

    angular after all

--- src/main.js
+++ src/main.js
@@ -1,38 +1,44 @@
 (function(){
 
-  var url = 'https://www.rijksmuseum.nl/api/en/collection';
-
-  var RijksTab = {
-
-    config: {
-      lang: 'en',
-      key: 'Z8SzSYqp',
-      format: 'json',
-      toppieces: true,
-      imgonly: true,
-      type: 'painting',
-      ps: 1
-    },
-
-    page: Math.floor(Math.random() * 4229),
-
-    fetch: function() {
-      return $.ajax({
-        url: url,
-        data: $.extend(this.config, {p: this.page})
-      });
-    },
-
-    init: function() {
-      this.fetch().then(function (arts){
-
-      });
-    }
-
+  var config = {
+    lang: 'en',
+    key: 'Z8SzSYqp',
+    format: 'json',
+    toppieces: true,
+    imgonly: true,
+    type: 'painting',
+    ps: 1
   }
 
-  var tab = Object.create(RijksTab);
+  var app = angular.module('RijksTab', ['ngResource']);
 
-  tab.fetch();
+  app.factory('artService', ['$resource', function ($resource) {
+
+    var resourceConfig = {
+      'query': {
+        method: 'GET',
+        transformResponse: function(response) {
+          var arts = angular.fromJson(response).artObjects;
+          return arts;
+        },
+        isArray: true
+      }
+    }
+
+    return $resource('https://www.rijksmuseum.nl/api/:lang/collection', {}, resourceConfig);
+
+  }]);
+
+  app.controller('TabController', ['$scope', '$http', 'artService', function ($scope, $http, artService) {
+
+    $scope.browserHeight = window.innerHeight;
+
+    var page = Math.floor(Math.random() * 4229);
+
+    artService.query(angular.extend(config, {p: page}), function (response) {
+      $scope.art = response[0];
+    });
+
+  }]);
 
 })();
