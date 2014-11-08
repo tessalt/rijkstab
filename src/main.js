e5e61eabfb0009b8037109bdd3520f7be8e29a61
Tessa Thornton <tessathornton@gmail.com>
2014-11-07 23:26:10 -0500

    styles and backtracking

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@
 
     function convertImg(url) {
       var deferred = $q.defer();
+      // var colorThief = new ColorThief();
       var canvas = document.createElement('CANVAS'),
           ctx = canvas.getContext('2d'),
           img = new Image;
@@ -23,11 +24,21 @@
       img.src = url;
       img.onload = function() {
         var dataURL;
+        var color;
         canvas.height = img.height;
         canvas.width = img.width;
         ctx.drawImage(img, 0, 0);
+        // var ct = new CanvasImage(canvas, ctx, img.width, img.height);
         dataURL = canvas.toDataURL('image/jpeg');
-        deferred.resolve(dataURL);
+        // try {
+        //   color = colorThief.getColor(ct);
+        // } catch (e) {
+        //   color = [0,0,0];
+        // }
+        deferred.resolve({
+          dataUrl: dataURL
+          // color: color
+        });
         canvas = null;
       }
       return deferred.promise;
@@ -56,48 +67,64 @@
 
   app.factory('imgService', ['artApiService', 'imgEncodeService', '$q', function (artApiService, imgEncodeService, $q) {
 
-    function setImg() {
+    function fetchNewImg() {
       var deferred = $q.defer();
-      chrome.storage.local.get('artwork', function (data) {
-
-        var today = moment(new Date());
-        var updated = moment(JSON.parse(data.artwork.lastUpdated));
-        var imgExpired = !updated.isSame(today, 'minute');
-        var page = Math.floor(Math.random() * 4229);
-        if (imgExpired) {
-
-          artApiService.query(angular.extend(config, {p: page})).$promise.then(function (response){
-
-            var imgData = response[0];
-            var url = imgData.webImage.url;
-
-            imgEncodeService(url).then(function (data) {
-              imgData.dataUrl = data;
-              imgData.lastUpdated = JSON.stringify(new Date());
-              chrome.storage.local.set({artwork: imgData}, function (){
-                deferred.resolve(imgData);
-              });
-            });
-
+      var page = Math.floor(Math.random() * 4229);
+      artApiService.query(angular.extend(config, {p: page})).$promise.then(function (response){
+        var imgData = response[0];
+        var url = imgData.webImage.url;
+        imgEncodeService(url).then(function (data) {
+          imgData.dataUrl = data.dataUrl;
+          imgData.mainColor = data.color;
+          imgData.lastUpdated = JSON.stringify(new Date());
+          chrome.storage.local.set({artwork: imgData}, function (){
+            deferred.resolve(imgData);
           });
-
-        } else {
-          deferred.resolve(data.artwork);
-        }
-
+        });
       });
       return deferred.promise;
     }
 
-    return setImg;
+    function setImg() {
+      var deferred = $q.defer();
+      chrome.storage.local.get('artwork', function (data) {
+        var today = moment(new Date());
+        var updated = moment(JSON.parse(data.artwork.lastUpdated));
+        var imgExpired = !updated.isSame(today);
+        if (imgExpired) {
+          fetchNewImg().then(function (response){
+            deferred.resolve(response);
+          });
+        } else {
+          deferred.resolve(data.artwork);
+        }
+      });
+      return deferred.promise;
+    }
+
+    return {
+      setImg: setImg,
+      fetchNewImg: fetchNewImg
+    };
 
   }]);
 
   app.controller('TabController', ['$scope', '$http', 'imgService', function ($scope, $http, imgService) {
 
+    chrome.topSites.get(function (data){
+      $scope.topSites = data;
+      console.log($scope.topSites);
+    });
+
     $scope.browserHeight = window.innerHeight;
 
-    imgService().then(function(data){
+    $scope.newImg = function() {
+      imgService.fetchNewImg().then(function (data){
+        $scope.art = data;
+      })
+    }
+
+    imgService.setImg().then(function (data){
       $scope.art = data;
     });
 
